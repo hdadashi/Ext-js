88109c32fabe2ebd9f10d50fe51769d559288f4b
hdadashi <hossein.dadashi73@gmail.com>
2022-11-26 10:37:29 +0330

    latest commit

--- app/desktop/src/view/setting/SettingView.js
+++ app/desktop/src/view/setting/SettingView.js
@@ -13,11 +13,13 @@ Ext.define('ModernApp.view.setting.setting', {
       html: `<h2 class="headerSetting">Change Information</h2>
 
       <form action="/actionPage" method="post">
-        <label for="fname" style='color: black'>First name:</label><br>
+      
+       <label for="fname" style='color: black'><span class="required">*</span> First name:</label><br>
         <input type="text" id="fname" name="fname" value="" placeholder = "Enter your firstname" required><br><br>
-        <label for="lname" style='color: black'>Last name:</label><br>
+        <label for="lname" style='color: black'><span class="required">*</span> Last name:</label><br>
         <input type="text" id="lname" name="lname" value="" placeholder = "Enter your lastname" required><br><br>
         <button class="buttonSetting"><span>Submit</span></button>
+        </div>
       </form>`,
       style: 'background-color: #e1e2e1'
     },
